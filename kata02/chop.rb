4683791a6f40c583d2fd450b795c4f86d1c3140a
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-10-04 22:03:29 +0200

    recursive chop

--- kata02/chop.rb
+++ kata02/chop.rb
@@ -1,6 +1,7 @@
 module Chop
 
-  def chop(number, array)
+  # iterative chop
+  def chop_iterative(number, array)
     return -1 if array.empty?
 
     left = 0
@@ -19,6 +20,27 @@ module Chop
     end while left<=right
 
     -1 #not found
+  end
+
+  # recursive chop
+  def chop(number, array)
+    return -1 if array.empty?
+
+    return 0 if array.size == 1 && array[0] == number
+    return -1 if array.size == 1 && array[0] != number
+
+    middle = array.size / 2
+
+    if array[middle]==number
+      return middle
+    elsif number > array[middle]
+      result = chop(number, array.slice(middle, array.size - middle))
+      return result < 0 ? -1 : middle+result
+    else
+      result = chop(number, array.slice(0, middle))
+      return result < 0 ? -1 : result
+    end
+
   end
 
 end
